1a82b64a5c4dda4cae1b3e41717476509b6567c7
quantumshiro <hiro.1nakanishi@gmail.com>
2024-01-29 03:54:07 +0900

    add: quaternion struct definition

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,11 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+const this = @This();
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
+pub const quaternion = struct {
+    x: f32,
+    i: f32,
+    j: f32,
+    k: f32,
+};
