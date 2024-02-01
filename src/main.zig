99bb5a3084bf67724ffd99adeae69c0a7d964904
quantumshiro <hiro.1nakanishi@gmail.com>
2024-02-01 22:40:47 +0900

    add: quaternion multiplication function

--- src/main.zig
+++ src/main.zig
@@ -9,3 +9,13 @@ pub const quaternion = struct {
     j: f32,
     k: f32,
 };
+
+// quaternion multiplications
+pub fn mul(lhs: quaternion, rhs: quaternion) quaternion {
+    return quaternion{
+        .x = lhs.i * rhs.j - lhs.j * rhs.i + lhs.k * rhs.x + lhs.k * rhs.x,
+        .i = lhs.j * rhs.x - lhs.x * rhs.j + lhs.k * rhs.i + lhs.k * rhs.i,
+        .j = lhs.x * rhs.i - lhs.i * rhs.x + lhs.k * rhs.j + lhs.k * rhs.j,
+        .k = lhs.k * rhs.k - lhs.x * rhs.x - lhs.i * rhs.i - lhs.j * rhs.j,
+    };
+}
