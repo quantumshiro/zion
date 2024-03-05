4a83dc23382018a40feeec95e59508b7bcc42638
quantumshiro <hiro.1nakanishi@gmail.com>
2024-03-05 22:08:04 +0900

    add: 多倍長に対応したquaternionのuintとdeinitの実装

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const testing = std.testing;
 const assert = std.debug.assert;
+const bigInt = std.math.big.int;
 
 pub const quaternion = struct {
     x: f32,
@@ -371,4 +372,44 @@ test "polynomial evaluation" {
 
     // 期待される結果は 7（2^2 + 2 + 1）
     try testing.expectEqual(quaternion{ .x = 7.0, .i = 0.0, .j = 0.0, .k = 0.0 }, result);
+}
+
+pub const multi_length_quaternion = struct {
+    x: bigInt.Managed,
+    i: bigInt.Managed,
+    j: bigInt.Managed,
+    k: bigInt.Managed,
+
+    const this = @This();
+
+    pub fn deinit(self: *this) void {
+        self.x.deinit();
+        self.i.deinit();
+        self.j.deinit();
+        self.k.deinit();
+    }
+
+    pub fn uint(allocator: std.mem.Allocator) !this {
+        return multi_length_quaternion{
+            .x = try bigInt.Managed.initSet(allocator, 1),
+            .i = try bigInt.Managed.initSet(allocator, 0),
+            .j = try bigInt.Managed.initSet(allocator, 0),
+            .k = try bigInt.Managed.initSet(allocator, 0),
+        };
+    }
+};
+
+test "multi length quaternion" {
+    var q = try multi_length_quaternion.uint(std.testing.allocator);
+    defer q.deinit();
+
+    var expect_one = try bigInt.Managed.initSet(std.testing.allocator, 1);
+    var expect_zero = try bigInt.Managed.initSet(std.testing.allocator, 0);
+    defer expect_one.deinit();
+    defer expect_zero.deinit();
+
+    try testing.expect(bigInt.Managed.eql(q.x, expect_one));
+    try testing.expect(bigInt.Managed.eql(q.i, expect_zero));
+    try testing.expect(bigInt.Managed.eql(q.j, expect_zero));
+    try testing.expect(bigInt.Managed.eql(q.k, expect_zero));
 }
